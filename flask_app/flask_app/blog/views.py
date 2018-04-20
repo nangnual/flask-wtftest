5242f9cf68b61db0f41dd9b92e56e31b84799dcb
Yanuar Arifin <3326885+nangnual@users.noreply.github.com>
2018-04-20 08:25:12 +0700

    change returned value to render

--- flask_app/flask_app/blog/views.py
+++ flask_app/flask_app/blog/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
 
-blueprint = Blueprint('blog', __name__, url_prefix='/blog', static_folder='./static')
+blueprint = Blueprint('blog', __name__, url_prefix='/blog', template_folder = 'templates')
 
 @blueprint.route('/create')
 def create():
+	# return blueprint.root_path
+	return render_template('something.html')
 	return 'blog.create'
